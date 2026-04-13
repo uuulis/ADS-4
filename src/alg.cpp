e530992b9c159110f3feed7e51f96422ff8c1db1
uuulis <listratovaul@gmail.com>
2026-04-13 19:39:37 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,84 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (arr[i] + arr[j] == value) {
+                count++;
+            }
+        }
+    }
+    return count;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    int left = 0;
+    int right = len - 1;
+    
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == value) {
+            int leftCount = 1;
+            int rightCount = 1;
+            while (left + 1 < right && arr[left] == arr[left + 1]) {
+                leftCount++;
+                left++;
+            }
+            while (right - 1 > left && arr[right] == arr[right - 1]) {
+                rightCount++;
+                right--;
+            }
+            
+            count += leftCount * rightCount;
+            left++;
+            right--;
+        } else if (sum < value) {
+            left++;
+        } else {
+            right--;
+        }
+    }
+    return count;
 }
+
+int binarySearch(int *arr, int left, int right, int target) {
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
+
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+    int count = 0;
+    
+    for (int i = 0; i < len; i++) {
+        int target = value - arr[i];
+        int pos = binarySearch(arr, i + 1, len - 1, target);
+        if (pos != -1) {
+            int targetCount = 1;
+            int j = pos - 1;
+            while (j > i && arr[j] == target) {
+                targetCount++;
+                j--;
+            }
+            j = pos + 1;
+            while (j < len && arr[j] == target) {
+                targetCount++;
+                j++;
+            }
+            count += targetCount;
+            i += (arr[i + 1] == arr[i] ? 0 : 0);
+        }
+        while (i + 1 < len && arr[i] == arr[i + 1]) {
+            i++;
+        }
+    }
+    return count;
 }
