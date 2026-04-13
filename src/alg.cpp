ddffd6f4438207789af32f29457113479303ef90
uuulis <listratovaul@gmail.com>
2026-04-13 21:30:25 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -46,26 +46,26 @@ int countPairs2(int *arr, int len, int value) {
     return count;
 }
 
-int binarySearch(int *arr, int left, int right, int target) {
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == target) {
-            return mid;
-        } else if (arr[mid] < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-    return -1;
-}
-
 int countPairs3(int *arr, int len, int value) {
     int count = 0;
     
     for (int i = 0; i < len; i++) {
         int target = value - arr[i];
-        int pos = binarySearch(arr, i + 1, len - 1, target);
+        int left = i + 1;
+        int right = len - 1;
+        int pos = -1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] == target) {
+                pos = mid;
+                break;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
         
         if (pos != -1) {
             int targetCount = 1;
